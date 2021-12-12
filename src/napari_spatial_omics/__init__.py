beb91a07e65a480ad554719f647b4d6bae08af22
Sebastian Gonzalez Tirado <sebgoti8@gmail.com>
2021-12-12 12:20:12 +0100

    Created working version using dock_widget

--- src/napari_spatial_omics/__init__.py
+++ src/napari_spatial_omics/__init__.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 
 from ._reader import napari_get_reader
 
-
-from ._function import napari_experimental_provide_function
+from ._dock_widget_original import napari_experimental_provide_dock_widget
+#from ._function import napari_experimental_provide_function
